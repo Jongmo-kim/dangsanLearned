0fab0c383477a6cf3af6c5733391bb2a66766f01
wa1358@naver.com <wa1358@naver.com>
2020-10-12 15:58:02 +0900

    20-10-12 수업 및 개인 프로젝트 분리(잦은 충돌로 인해)

---
+++ Test/src/kr/test/start/Start.java
@@ -0,0 +1,15 @@
+package kr.test.start;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Start{
+	public static void main(String args[]) {
+		ArrayList<String> words = new ArrayList<String>(); 
+		words.add("gone");
+		words.add("go");
+		words.add("golf");
+		
+		
+	}
+}
